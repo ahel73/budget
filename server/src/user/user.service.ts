79de5d46ec3c2154db790b532c13482b868e61da
Нагаев Алексей <nag495@yandex.ru>
2023-09-16 15:31:53 +0300

    5 сделали регистрацию пользователя

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,26 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import * as argon2 from "argon2";
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return `This action adds a new user ${createUserDto}`;
-  }
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) { }
 
-  findAll() {
-    return `This action returns all user`;
+  async create(createUserDto: CreateUserDto) {
+    const existUser = await this.userRepository.findOne({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+    if (existUser) {
+      const mess = 'Пользователь с такой почтой уже существует';
+      throw new BadRequestException(mess);
+    }
+
+    const user = await this.userRepository.save({
+      email: createUserDto.email,
+      password: await argon2.hash(createUserDto.password),
+    });
+
+    return user;
   }
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user ${updateUserDto}`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }
