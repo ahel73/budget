68016a4b5e66bf7d65c1a7169a1da10706f1ff04
Нагаев Алексей <nag495@yandex.ru>
2023-10-29 20:16:25 +0300

    11 Лайаут Шапка навигация 404

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,7 +1,16 @@
 import { FC } from 'react';
+import img from '../assets/404.png'
+import {Link} from "react-router-dom";
 
 const ErrorPage: FC = () => {
-    return <div>ErrorPage</div>
+    return (
+        <div className='flex min-h-screen flex-col items-center justify-center gap-10 bg-slate-900 font-roboto text-white'>
+            <img src={img} alt='' />
+            <Link to={'/'} className='rounded-md bg-sky-500 px-6 py-2 hover:bg-sky-600' >
+                Back
+            </Link>
+        </div>
+    )
 }
 
 export default ErrorPage;
