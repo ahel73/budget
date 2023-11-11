d1d3f3a91b4cd429f79901f0ed7c4e636a7a57a7
Нагаев Алексей <nag495@yandex.ru>
2023-11-11 16:18:49 +0300

    11 Исправил опечатку

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,6 +1,6 @@
 import { useAppSelector } from '../store/hooks'
 
 export const useAutn = (): boolean => {
-    const isAuth = useAppSelector((state) => state.user.isAth);
+    const isAuth = useAppSelector((state) => state.user.isAuth);
     return isAuth;
 }
